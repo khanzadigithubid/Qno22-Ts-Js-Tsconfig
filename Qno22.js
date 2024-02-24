23ac2e2b6a70c162b535f062ff69f1601b244252
Khanzadi Wazir Ali <160390882+khanzadigithubid@users.noreply.github.com>
2024-02-24 13:57:12 +0500

    Qno22 Ts/Js/Tsconfig and node js of 45 Assignments
    Qno22 Ts/Js/Tsconfig and node js of 45 Assignments

---
+++ Qno22.js
@@ -0,0 +1,7 @@
+"use strict";
+/*22. Intentional Error: If you haven’t received an array index error in one of your
+programs yet, try to make one happen. Change an index in one of your programs to produce
+an index error. Make sure you correct the error before closing the program.*/
+let months = ["January", "February", "March", "April", "May"];
+console.log(months[5]);
+console.log(months[3]);
